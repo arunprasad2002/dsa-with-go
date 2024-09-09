cce766414786ba625d7e7d36e3f10335bbc1bd21
arun <ptech784@gmail.com>
2024-09-09 21:13:22 +0530

    solved -  using string std pacakage

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -1,21 +1,20 @@
 package patterns
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+// PrintTriangle prints a right-angled triangle of '*' characters.
 func PrintTriangle(number int) {
-	for i := 0; i <= number; i++ {
-		for j := 0; j <= i; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println("")
+	for i := 1; i <= number; i++ {
+		fmt.Println(strings.Repeat("*", i))
 	}
 }
 
+// PrintTriangleReverse prints an inverted right-angled triangle of '*' characters.
 func PrintTriangleReverse(number int) {
-	for i := number; i >= 0; i-- {
-		for j := 0; j <= i; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println("")
+	for i := number; i >= 1; i-- {
+		fmt.Println(strings.Repeat("*", i))
 	}
 }
